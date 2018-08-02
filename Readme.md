1fb90a38b533f7992de7906f3b2935a5cc1f8ea7
唐芳华 <fanghua.t@thelook.cn>
2018-08-02 16:38:09 +0800

    import

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# sp1-keycloak
+
+spring boot 1.5.3 + keycloak 4.1.0 demo
---
+++ src/main/java/demo/sp1keycloak/Controller1.java
@@ -0,0 +1,40 @@
+/*
+ *  copyright (c) thelook.cn
+ */
+package demo.sp1keycloak;
+
+import java.security.Principal;
+import java.util.Objects;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author 老唐
+ */
+@RestController
+public class Controller1 {
+
+    @RequestMapping("/")
+    public String index() {
+        return "hello";
+    }
+
+    @RequestMapping("/user")
+    public String user(Principal p) {
+        Objects.requireNonNull(p);
+        final StringBuffer sb = new StringBuffer();
+        sb.append("user:").append(p.getName()).append("\n");
+        sb.append("role:").append(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+        return sb.toString();
+    }
+
+    @RequestMapping("/logout")
+    public String logout(HttpServletRequest req) throws ServletException {
+        req.logout();
+        return null;
+    }
+}
